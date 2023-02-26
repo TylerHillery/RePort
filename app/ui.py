93d74cb07d5a4e26d220336990c8ecc52ad3a782
Tyler Hillery <tyler.p.hillery@outlook.com>
2023-02-26 12:22:49 -0600

    move code from main to ui

--- app/ui.py
+++ app/ui.py
@@ -80,34 +80,7 @@ class CashInput():
 
             db.query("DELETE FROM cash")
             db.query("INSERT INTO cash SELECT * FROM df")
-        return None
-
-    def form():
-        form  = st.form("cash_input")
-        c1, c2, c3  = form.columns((1.5,1.5,1.5))
-        operation   = c1.selectbox('Select Operation',('Add','Update','Delete'))
-        account     = c2.text_input('Account Name')
-        cash        = c3.number_input('Investable Cash ($)',min_value = 0.00)
-        submitted   = form.form_submit_button("Submit")
-
-        data = (
-            operation,
-            account.strip(),
-            cash
-        )
-
-        if submitted:
-            if operation == "Add":
-                parms = [data[1:]]
-            if operation == "Delete":
-                parms = [(data[1],)]
-            if operation == "Update":
-                parms = [data[:0:-1],]
-
-            db.crud(operation,'cash',parms)
-
-        return data
-        
+        return None        
 
 class HoldingsInput():   
     def file():
@@ -132,65 +105,11 @@ class HoldingsInput():
             """)
         return None 
 
-    def form():
-        form  = st.form("holdings_input")
-        row_1 = form.container()
-        row_2 = form.container()
-        
-        with row_1:
-            c1, c2, c3  = row_1.columns((1.5,1.5,1.5))
-            operation   = c1.selectbox('Select Operation',('Add','Update','Delete'))
-            account     = c2.text_input('Account Name')
-            ticker      = c3.text_input('Ticker')
-        
-        with row_2:
-            c1, c2, c3, c4  = row_2.columns((1.25,1.25,1.25,1.25))
-            shares          = c1.number_input('Shares',min_value = 0.00)
-            target          = c2.number_input('Target Weight (%)')
-            cost            = c3.number_input(
-                                'Cost', 
-                                min_value = 0.00, 
-                                help = "Total Cost for all shares"
-                            )
-
-            price           = c4.number_input(
-                                'Price',
-                                min_value = 0.00,
-                                help = """
-                                Current stock price or price you plan on
-                                purchasing the stock at
-                                """
-                            ) 
-        
-        submitted = form.form_submit_button("Submit")
-
-        data = (
-            operation,
-            md5((account.strip() + ticker.strip()).lower().encode('utf-8')).hexdigest(),
-            account.strip(),
-            ticker.strip(),
-            shares,
-            target,
-            cost,
-            price
-        )
-        
-        if submitted:
-            if operation == "Add":
-                parms = [data[1:]]
-            if operation == "Delete":
-                parms = [(data[1],)]
-            if operation == "Update":
-                parms = [data[4:] + (data[1],)]
-            
-            db.crud(operation,'holdings',parms)
-        
-        return data
-
 class Portfolio():
     def create_tables(rebalance_type, is_frac_shares):
         db.query(get_query_string('create_holdings_table')) 
-        db.query(get_query_string('create_cash_table')) 
+        db.query(get_query_string('create_cash_table'))
+        Portfolio.create_future_holdings(rebalance_type,is_frac_shares)
 
     def update_tables(table: tuple,df: pd.DataFrame) -> None:
         if table == 'cash':
@@ -389,3 +308,255 @@ class Portfolio():
     def get_account_future_cash(account: str,rebalance_type, is_frac_shares) -> float: 
         df = Portfolio.get_future_holdings_table(rebalance_type, is_frac_shares)
         return db.fetch(f"SELECT max(cash) FROM df WHERE account_name = '{account}'",return_df=False)[0][0]
+
+    def metrics(accounts,index):
+        c1,c2,c3,c4 = st.columns([2,2,2,2,])
+        raw_holdings_df = db.fetch(get_query_string("select_holdings"))
+        raw_future_holdings_df = db.fetch(get_query_string("select_future_holdings"))
+        holdings_df = db.fetch(
+            f"""
+            SELECT *
+            FROM raw_holdings_df
+            WHERE account_name = '{accounts[index]}'
+            """
+        )
+        
+        future_holdings_df = db.fetch(
+            f"""
+            SELECT *
+            FROM raw_future_holdings_df
+            WHERE account_name = '{accounts[index]}'
+            """
+        )
+
+        cash = db.fetch("SELECT max(cash) FROM holdings_df",return_df=False)[0][0]
+        cash_formatted = f"${cash:,.2f}"
+        c1.metric("Investable Cash",cash_formatted)
+
+
+        future_cash = db.fetch("SELECT max(cash) FROM future_holdings_df",return_df=False)[0][0]
+        future_cash_formatted = f"${future_cash:,.2f}"
+        
+        cash_delta = f"{future_cash - cash:,.2f}"
+        c2.metric("Investable Cash After Rebalance",
+                  future_cash_formatted,delta=cash_delta)
+
+        market_value = db.fetch("SELECT sum(market_value) FROM holdings_df",
+                                return_df=False)[0][0]
+        market_value_formatted = f"${market_value:,.2f}"
+
+        gain_loss = db.fetch("SELECT sum(market_value) - sum(cost) FROM holdings_df",
+                             return_df=False)[0][0]
+        gain_loss_formatted = f"{gain_loss:,.2f}"
+        c3.metric("Account Market Value",
+                  market_value_formatted,
+                  delta=gain_loss_formatted)
+
+        gain_loss_pct = db.fetch("SELECT (sum(market_value) - sum(cost))/sum(cost) FROM holdings_df",
+                                 return_df=False)[0][0]
+        gain_loss_pct_formatted = f"{gain_loss_pct*100:,.2f}%"
+        c4.metric("Account Gain/Loss (%)",gain_loss_pct_formatted)
+
+        return None
+
+    def color_negative_red(value):
+        """
+        Colors elements in a date frame
+        green if positive and red if
+        negative. Does not color NaN
+        values.
+        """
+
+        if value < 0:
+            color = 'red'
+        elif value > 0:
+            color = 'green'
+        else:
+            color = 'black'
+
+        return f'{color}: %s'
+
+    def dataframes(accounts,index):
+        c1, c2 = st.columns([2,2])
+        raw_holdings_df = db.fetch(get_query_string("select_holdings"))
+        raw_future_holdings_df = db.fetch(get_query_string("select_future_holdings"))
+        holdings_df = db.fetch(
+            f"""
+            SELECT *
+            FROM raw_holdings_df
+            WHERE account_name = '{accounts[index]}'
+            """
+        )
+        
+        future_holdings_df = db.fetch(
+            f"""
+            SELECT *
+            FROM raw_future_holdings_df
+            WHERE account_name = '{accounts[index]}'
+            """
+        )    
+        holdings_df_columns = {
+                            "account_name"  : "Account",
+                            "ticker"        : "Ticker",
+                            "shares"        : "Shares",
+                            "price"         : "Price",
+                            "market_value"  : "Market Value",
+                            "cost"          : "Cost",
+                            "gain_loss"     : "Gain/Loss",
+                            "gain_loss_pct" : "Gain/Loss %",
+                            "target_weight" : "Target Weight",
+                            "current_weight": "Current Weight",
+                            "target_diff"   : "Target Diff"
+
+                        }
+
+        c1.markdown("**Before Rebalance**")
+        c1.dataframe(holdings_df.loc[:,list(holdings_df_columns.keys())]
+                        .rename(columns=holdings_df_columns)
+                        .style
+                        .applymap(Portfolio.color_negative_red, 
+                                  subset=['Target Diff',"Gain/Loss","Gain/Loss %"])
+                        .format({
+                            'Target Weight':    lambda x: f"{x:,.2f}%",
+                            'Current Weight':   lambda x: f"{x:,.2f}%",
+                            'Target Diff':      lambda x: f"{x:,.2f}%",
+                            'Gain/Loss %':      lambda x: f"{x:,.2f}%",
+                            'Cost':             lambda x: f"${x:,.2f}",
+                            'Market Value':     lambda x: f"${x:,.2f}",
+                            'Price':            lambda x: f"${x:,.2f}",
+                            'Gain/Loss':        lambda x: f"${x:,.2f}",
+                            'Shares':           lambda x: f"{x:,.4f}"    
+                        }))
+        c2.markdown("**After Rebalance**")
+        c2.dataframe(future_holdings_df.loc[:,list(holdings_df_columns.keys())]
+                        .rename(columns=holdings_df_columns)
+                        .style
+                        .applymap(Portfolio.color_negative_red, 
+                                  subset=['Target Diff',"Gain/Loss","Gain/Loss %"])
+                        .format({
+                            'Target Weight':    lambda x: f"{x:,.2f}%",
+                            'Current Weight':   lambda x: f"{x:,.2f}%",
+                            'Target Diff':      lambda x: f"{x:,.2f}%",
+                            'Gain/Loss %':      lambda x: f"{x:,.2f}%",
+                            'Cost':             lambda x: f"${x:,.2f}",
+                            'Market Value':     lambda x: f"${x:,.2f}",
+                            'Price':            lambda x: f"${x:,.2f}",
+                            'Gain/Loss':        lambda x: f"${x:,.2f}",
+                            'Shares':           lambda x: f"{x:,.4f}"    
+                        }))
+        
+        return None
+
+    def visuals(accounts,index):
+        c1,c2 = st.columns([7,3])
+        raw_holdings_df = db.fetch(get_query_string("select_holdings"))
+        raw_future_holdings_df = db.fetch(get_query_string("select_future_holdings"))
+        holdings_df = db.fetch(
+            f"""
+            SELECT *
+            FROM raw_holdings_df
+            WHERE account_name = '{accounts[index]}'
+            """
+        )
+        
+        future_holdings_df = db.fetch(
+            f"""
+            SELECT *
+            FROM raw_future_holdings_df
+            WHERE account_name = '{accounts[index]}'
+            """
+        )   
+        combined_df = db.fetch(
+            f"""
+            SELECT
+                'Before' as rebalance,
+                ticker,
+                shares,
+                target_weight,
+                current_weight,
+                target_diff,
+                cost,
+                market_value,
+                price,
+                gain_loss,
+                gain_loss_pct,
+            FROM
+                holdings_df
+            WHERE account_name = '{accounts[index]}'
+            UNION
+            SELECT
+                'After' as rebalance,
+                ticker,
+                shares,
+                target_weight,
+                current_weight,
+                target_diff,
+                cost,
+                market_value,
+                price,
+                gain_loss,
+                gain_loss_pct,
+            FROM
+                future_holdings_df
+            WHERE account_name = '{accounts[index]}'
+            """
+        )
+
+        vega_chart = {
+            "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+            "mark": "bar",
+            "encoding": {
+                "x": {"field": "ticker","title" :"Ticker"},
+                "y": {"field": "target_diff", "type": "quantitative","title" :"Diff From Target Weight (%)"},
+                "tooltip": [
+                    {"field": "ticker", "type": "nominal", "title": "Ticker"},
+                    {"field": "target_weight", "type": "quantitative", "title":"Target Weight (%)"},
+                    {"field": "target_diff", "type": "quantitative","title":"Target Diff (%)"}
+                ],
+                "xOffset": {"field": "rebalance", "sort": "descending"},
+                "color": {
+                    "field": "rebalance", 
+                    "sort": "descending",
+                    "title":"After or Before Rebalance"
+                }   
+            }
+        }
+            
+        c1.markdown('<div style="text-align: center;"><strong>Comparison of Target Weight Differences Before vs After Rebalance</strong></div>', unsafe_allow_html=True)
+        
+        c1.vega_lite_chart(combined_df,vega_chart,use_container_width=True)
+
+        orders_df = db.fetch(
+            """
+            SELECT
+                CASE
+                    WHEN (future.shares - current.shares) > 0 then 'Buy'
+                    ELSE 'Sell'
+                END AS "Order Type",
+                current.ticker AS Ticker,
+                future.shares - current.shares AS Shares,
+                current.price AS Price,
+                (future.shares - current.shares) * -1 * current.price AS "Trade Amount"
+            FROM
+                holdings_df AS current
+                INNER JOIN future_holdings_df AS future using(holding_id)
+            WHERE
+                (future.shares - current.shares) != 0           
+            ORDER BY
+                Ticker
+            """
+        )
+        c2.markdown(
+            '''
+            <div style="text-align: center;"><strong>Orders Needed For Rebalance</strong></div>
+            ''',
+            unsafe_allow_html=True)
+        c2.dataframe(orders_df.style
+                            .applymap(Portfolio.color_negative_red, 
+                                      subset=["Trade Amount"])
+                            .format({
+                            'Price':            lambda x: f"${x:,.2f}",
+                            'Trade Amount':     lambda x: f"${x:,.2f}",
+                            'Shares':           lambda x: f"{x:,.4f}"    
+                        }))
+        return None
